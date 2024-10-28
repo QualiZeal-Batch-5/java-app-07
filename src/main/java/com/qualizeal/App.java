ccd32efdfe61fd52e96f585c38847c97a52cc8ce
Dhana <dhanaposimsetti89@gmail.com>
2024-10-28 15:40:41 +0530

    Added Code

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp07.operation.PalindromeNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		PalindromeNumber palindromeNumber = new PalindromeNumber();
+		String result = palindromeNumber.verifyIfNumberIsPalindromeOrNot(121);
+		System.out.println(result);
 	}
 }
