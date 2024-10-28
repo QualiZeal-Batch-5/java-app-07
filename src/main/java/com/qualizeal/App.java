05ca36cc2f09c871c5ef6b0c48cbcd5ef9c681f5
Ramya Mudragalla <mudragallaramya28@gmail.com>
2024-10-28 18:00:03 +0530

    Palindrome

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp07.operation.PalindromeNumber;
 public class App {
 	public static void main(String[] args) {
+		PalindromeNumber palindromeNumber = new PalindromeNumber();
+		String result = palindromeNumber.verifyIfNumberIsPalindromeOrNot(120);
+		
+		
+		System.out.println(result);
 		
 	}
 }
