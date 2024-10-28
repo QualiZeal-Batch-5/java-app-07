d9d10d41ce19ca88f5aa4f835fced641e668adc9
Samshritha <samshritha675pendem@gmail.com>
2024-10-28 17:48:17 +0530

    Added source file

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp07.operation.PalindromeNumber;
+
 public class App {
 	public static void main(String[] args) {
+		PalindromeNumber palindromeNumber = new PalindromeNumber();
+		palindromeNumber.verifyIfNumberIsPalindromeOrNot(44);
 		
 	}
 }
