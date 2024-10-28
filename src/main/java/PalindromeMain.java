d8bc52054efda85057990939d9d1a13d4cca3d96
Sri Harika <nookalasriharika@gmail.com>
2024-10-28 18:31:30 +0530

    Palindrome

---
+++ src/main/java/PalindromeMain.java
@@ -0,0 +1,9 @@
+package com.qualizeal.javaapp07.operation;
+
+public class PalindromeMain {
+	public static void main(String args[]) {
+		PalindromeNumber palindromeNumber = new PalindromeNumber();
+		System.out.println(palindromeNumber.verifyIfNumberIsPalindromeOrNo(555));
+	}
+
+}
---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package com.qualizeal.javaapp07.operation;
+
+public class PalindromeNumber {
+	public String definePalindromeNumber() {
+		return "Palindrome";
+	}
+
+	public String verifyIfNumberIsPalindromeOrNo(int num) {
+		String s = "";
+		if (num < 0) {
+			s = "-1";
+		} else if (num == 0) {
+			s = "-2";
+		} else if (num < 10) {
+			s = "-3";
+
+		} else if (num >= 10) {
+			int sum = 0, r;
+			int temp = num;
+			while (num > 0) {
+				r = num % 10;
+				sum = (sum * 10) + r;
+				num = num / 10;
+			}
+			if (temp == sum) {
+				s = definePalindromeNumber();
+			} else {
+				s = "Not a Palindrome Number";
+			}
+		}
+		return s;
+	}
+}
